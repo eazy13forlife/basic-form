2e78f601a017c91a5161fda2a632533dabfb0d9f
Eric B <eric_baffour-addo@alumni.brown.edu>
2023-02-07 17:19:34 -0800

    Create boilerplate for project and initialize standard styling

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
-import ReduxThunk from "redux-thunk";
 
-import combinedReducers from "./reducers";
 import App from "./App.js";
 import "./styles/main.scss";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-  combinedReducers,
-  composeEnhancers(applyMiddleware(ReduxThunk))
-);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.querySelector("#root")
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
